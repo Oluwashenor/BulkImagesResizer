41366f02b6c9a47d356a8636a72f5af8d75df8da
Adesina <adeshiname@gmail.com>
2024-06-10 12:07:34 +0100

    Optimizations

--- BulkImagesResizer/Program.cs
+++ BulkImagesResizer/Program.cs
@@ -7,62 +7,88 @@ internal class Program
     {
         Chatting();
         List<string> validImageExtensions = [".bmp", ".jpeg", ".png"];
-        string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        // Console.WriteLine("User Directory: " + userDirectory);
-        Console.WriteLine(@"Please enter the location of your images using this format Desktop\Folder\Folder or Documents\Something\Something or Pictures\Something\Something");
-        string imagesLocation = Console.ReadLine();
-        string imagesPath = $@"{userDirectory}\{imagesLocation}"; ;
-        Console.WriteLine($"Is this the valid location of your images {imagesPath} ?");
-        if (Path.Exists(imagesPath))
+        var imagesPath = GetValidLocation();
+        Console.WriteLine("Folder Found");
+        string[] files = Directory.GetFiles(imagesPath);
+        if (files.Length < 1)
         {
-            Console.WriteLine("Folder Found");
-            string[] files = Directory.GetFiles(imagesPath);
-            if (files.Length < 1)
+            Console.WriteLine("This folder is empty");
+            return;
+        }
+        else
+        {
+            var validFiles = files.Where(x => validImageExtensions.Contains(Path.GetExtension(x).ToLower())).ToList();
+            if (validFiles.Count < 1)
             {
-                Console.WriteLine("This folder is empty");
+                Console.WriteLine("This folder contains no image");
                 return;
             }
             else
             {
-                var validFiles = files.Where(x => validImageExtensions.Contains(Path.GetExtension(x).ToLower())).ToList();
-                if (validFiles.Count < 1)
+                Console.WriteLine($"Your folder contains {validFiles.Count} Valid Images");
+                int count = 0;
+                var createFolder = Directory.CreateDirectory(Path.Combine(imagesPath, "compressed"));
+                if (!createFolder.Exists)
                 {
-                    Console.WriteLine("This folder contains no image");
+                    Console.WriteLine("Unable to Create your folder");
                     return;
                 }
-                else
+                foreach (var item in validFiles)
                 {
-                    Console.WriteLine($"Your folder contains {validFiles.Count} Valid Images");
-                    int count = 0;
-                    var createFolder = Directory.CreateDirectory(Path.Combine(imagesPath, "compressed"));
-                    if (!createFolder.Exists)
-                    {
-                        Console.WriteLine("Unable to Create your folder");
-                        return;
-                    }
-                    foreach (var item in validFiles)
-                    {
-                        count++;
-                        Console.WriteLine($"Processing file {count}");
-                        _ = ResizeImage(createFolder.FullName, item);
-                        Console.WriteLine($"Completed file {count}");
-                    }
+                    count++;
+                    Console.WriteLine($"Processing file {count}");
+                    _ = ResizeImage(createFolder.FullName, item);
+                    Console.WriteLine($"Completed file {count}");
                 }
             }
-        }
-        else
-        {
-            Console.WriteLine("You do not have this folder on your desktop");
         }
     }
 
     private static bool ResizeImage(string pathToSave, string imageFile)
     {
-        using Image image = Image.Load(imageFile);
-        image.Mutate(x => x.Resize(image.Width / 5, image.Height / 5));
-        string fileToSave = Path.Combine(pathToSave, Path.GetFileName(imageFile));
-        image.Save(fileToSave);
-        return true;
+        try
+        {
+            using Image image = Image.Load(imageFile);
+            image.Mutate(x => x.Resize(image.Width / 5, image.Height / 5));
+            string fileToSave = Path.Combine(pathToSave, Path.GetFileName(imageFile));
+            image.Save(fileToSave);
+            image.Dispose();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message.ToString());
+            return false;
+        }
+    }
+
+    private static string GetValidLocation()
+    {
+        string imagesPath = string.Empty;
+        var isValid = false;
+        while (!isValid)
+        {
+            string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Console.WriteLine("");
+            Console.WriteLine("***********************************");
+            Console.WriteLine(@"Please enter the location of your images using this format Desktop\Folder\Folder or Documents\Something\Something or Pictures\Something\Something");
+            string imagesLocation = Console.ReadLine();
+            imagesPath = $@"{userDirectory}\{imagesLocation}"; ;
+            Console.Write($"Is this the valid location of your images {imagesPath} ? ");
+            var userInput = Console.ReadLine();
+            if (new List<string> { "yes", "yea"}.Contains(userInput.ToLower()))
+            {
+                if (Path.Exists(imagesPath))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have this folder on your desktop");
+                }
+            }
+        }
+        return imagesPath;
     }
 
     private static void Chatting()
